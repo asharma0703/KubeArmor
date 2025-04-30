2827c2865ece864e9af9a28a12c1e8f225b2e21f
Arnav Sharma <arnav.sharma@mongodb.com>
2025-04-30 04:48:21 -0400

    Refactor visibility log generation and add unittest

--- KubeArmor/feeder/feeder_test.go
+++ KubeArmor/feeder/feeder_test.go
@@ -4,11 +4,13 @@
 package feeder
 
 import (
+	"flag"
 	"sync"
 	"testing"
 
 	cfg "github.com/kubearmor/KubeArmor/KubeArmor/config"
 	tp "github.com/kubearmor/KubeArmor/KubeArmor/types"
+	pb "github.com/kubearmor/KubeArmor/protobuf"
 )
 
 func TestFeeder(t *testing.T) {
@@ -17,6 +19,7 @@ func TestFeeder(t *testing.T) {
 	nodeLock := new(sync.RWMutex)
 
 	// load configuration
+	flag.CommandLine = flag.NewFlagSet("TestFeeder", flag.ContinueOnError)
 	if err := cfg.LoadConfig(); err != nil {
 		t.Log("[FAIL] Failed to load configuration")
 		return
@@ -36,4 +39,63 @@ func TestFeeder(t *testing.T) {
 		return
 	}
 	t.Log("[PASS] Destroyed logger")
+}
+
+func TestMarshalVisibilityLog(t *testing.T) {
+	flag.CommandLine = flag.NewFlagSet("TestMarhsalVisibilityLog", flag.ContinueOnError)
+	if err := cfg.LoadConfig(); err != nil {
+		t.Log("[FAIL] Failed to load configuration")
+		return
+	}
+	// example visibility log
+	visibilityLog := tp.Log{
+		ClusterName: "default",
+		// exclude hostname for unittest since it is set using node info from feeder
+		// HostName:          "kubearmor-dev2",
+		Type:              "HostLog",
+		Source:            "/usr/bin/dockerd",
+		Resource:          "/usr/bin/runc --version",
+		Operation:         "Process",
+		Data:              "syscall=SYS_EXECVE",
+		Result:            "Passed",
+		HostPID:           193088,
+		HostPPID:          914,
+		PID:               193088,
+		PPID:              914,
+		ParentProcessName: "/usr/bin/dockerd",
+		ProcessName:       "/usr/bin/runc",
+	}
+
+	expectedMarshaledLog := &pb.Log{
+		ClusterName: "default",
+		// HostName:          "kubearmor-dev2",
+		Type:              "HostLog",
+		Source:            "/usr/bin/dockerd",
+		Resource:          "/usr/bin/runc --version",
+		Operation:         "Process",
+		Data:              "syscall=SYS_EXECVE",
+		Result:            "Passed",
+		HostPID:           193088,
+		HostPPID:          914,
+		PID:               193088,
+		PPID:              914,
+		ParentProcessName: "/usr/bin/dockerd",
+		ProcessName:       "/usr/bin/runc",
+	}
+
+	// marshal visibility log and check result
+	marshaledLog := MarshalVisibilityLog(visibilityLog)
+	if marshaledLog != expectedMarshaledLog {
+		t.Logf("[FAIL] Expected marshaled log: %+v", expectedMarshaledLog)
+		t.Logf("but got: %+v", marshaledLog)
+	}
+
+	// do the same but with dropped resource field
+	cfg.GlobalCfg.DropResourceFromProcessLogs = true
+	expectedMarshaledLog.Resource = ""
+	marshaledLog = MarshalVisibilityLog(visibilityLog)
+	if marshaledLog != expectedMarshaledLog {
+		t.Logf("[FAIL] Expected marshaled log: %+v", expectedMarshaledLog)
+		t.Logf("but got: %+v", marshaledLog)
+	}
 }
